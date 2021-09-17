aabca21f56f682bf5f1f04960b6d4d2795854245
Richard <richokoye1@gmail.com>
2021-09-17 20:11:00 +0100

    frequency counter pattern

--- 5. Patterns/Frequency Counter/same.js
+++ 5. Patterns/Frequency Counter/same.js
@@ -42,34 +42,3 @@ function same(arr1, arr2) {
 
     return true;
  }
-
- // Challange 2
- // Write a functon to determine if the second string is an anagram of the first
- // An anagram is a word or phrase, or name formed by rearranging the letters of anothe
- // such as cinema fromed from iceman
-
- function validAnagram(first, second) {
-     if(first.length !== second.length) return false;
-
-     const lookup = {};
-
-     // loop through the first string
-     for(let i = 0; i < first.length; i++) {
-         let letter = first[i];
-         lookup[letter] = ++lookup[letter] || 1
-     }
-
-     // loop throung the second string
-     for(let i = 0; i < second.length; i++) {
-         let letter = second[i];
-
-         if(!lookup[letter]) {
-             return false;
-         } else {
-             --lookup[letter];
-         }
-     }
-     return true
- }
-
-console.log(validAnagram('aaz', 'zba'))
