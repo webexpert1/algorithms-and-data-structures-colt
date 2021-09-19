ea862b480a6ddcec17dae3572a1abdd2e45bde76
Richard <richokoye1@gmail.com>
2021-09-19 21:18:05 +0100

    recursion

--- 6. Recursion/collectOddValues.js
+++ 6. Recursion/collectOddValues.js
@@ -1,3 +1,4 @@
+// Collect Odd values - Helper method recursion
 function collectOddValues(array) {
     let result = [];
 
@@ -13,6 +14,20 @@ function collectOddValues(array) {
     helper(array)
 
     return result;
+}
+
+// Collect Odd values - Pure recursion
+function collectOddValues(array) {
+    let newArray = [];
+
+    if(array.length === 0) return newArray;
+    
+    if(array[0] % 2 !== 0) {
+        newArray.push(array[0]);
+    }
+
+    newArray = newArray.concat(collectOddValues(array.slice(1)))
+    return newArray;
 }
 
 console.log(collectOddValues([1,2,3,4,5,6,7,8,9]))
