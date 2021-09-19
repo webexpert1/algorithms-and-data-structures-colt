7366d66d5f47553ae06dbd00d1c2483006b4a8bd
Richard <richokoye1@gmail.com>
2021-09-19 20:16:45 +0100

    factorial

---
+++ 6. Recursion/factorial.js
@@ -0,0 +1,15 @@
+function factorial(num) {
+    let total = 1;
+    for(let i = num; i > 1; i--) {
+        total *= i;
+    }
+    return total;
+}
+
+// factorial - recursion
+function factorial(num) {
+    if(num === 1) return 1;
+    return num * factorial(num-1);
+}
+
+console.log(factorial(4))
