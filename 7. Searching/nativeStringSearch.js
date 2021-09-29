bc859c0e8c7934249a4b1b8dec21fd8b99cc63e1
Richard <richokoye1@gmail.com>
2021-09-29 02:24:49 +0100

    search

--- 7. Searching/nativeStringSearch.js
+++ 7. Searching/nativeStringSearch.js
@@ -11,7 +11,7 @@ function nativeStringSearch(longStr, shortStr) {
     for (let i = 0; i < longStr.length; i++) {
         for (let j = 0; j < shortStr.length; j++) {
             if (shortStr[j] !== longStr[i+j]) break;
-            if(j === shortStr.length - 1) ++count;
+            if (j === shortStr.length - 1) ++count;
         }
     }
      return count;
